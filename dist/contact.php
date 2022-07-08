3113bf90ea4b84abb8dfed7a08009c02eb8f7275
eweb-ossy <ossy2000@gmail.com>
2022-07-08 14:03:23 +0900

    fix

--- dist/contact.php
+++ dist/contact.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex">
     <title>お問い合わせ｜ゑびすグループ「満てん」「ゑびす」「牛粋」「ぶたと和いん」宮城県の飲食店グループ</title>
     <link rel="stylesheet" href="css/contact.css?<?= time() ?>">
 </head>
